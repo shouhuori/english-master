da7cdcd1604b152e16eb3d630a30f8b3acfbe543
nico <0224wc@gmail.com>
2020-02-22 15:23:15 +0800

    词汇量测试

--- screens/WordTestScreen.tsx
+++ screens/WordTestScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import {Text, Layout,Button,TopNavigation} from '@ui-kitten/components';
+import {Text, Layout,Button,TopNavigation,TopNavigationAction} from '@ui-kitten/components';
 import {
   Platform,
   Dimensions,
   StyleSheet,
   View,
+  Alert
 } from 'react-native';
 
 import * as SQLite from 'expo-sqlite';
@@ -12,7 +13,7 @@ import * as SQLite from 'expo-sqlite';
 const db  = SQLite.openDatabase('dict.db');
 
 
-const getRandomNumberByRangeWord = (start, end) => {
+const getRandomNumberByRange = (start, end):number => {
   return   Math.floor(Math.random() * (end - start) + start)
 }
 
@@ -25,12 +26,14 @@ const getRandomNumberByRangeWord = (start, end) => {
  * 词汇量测试
  */
 export default  function WordTestScreen(props) {
-  const [num, setNum] = React.useState(0);
-  const [word, setWord] = React.useState(0);
-  let id = getRandomNumberByRangeWord(5000,5100);
+  const [num, setNum] = React.useState(0); // 词汇量
+  const [order, setOrder] = React.useState(0); // 题目次数
+  const [word, setWord] = React.useState({name:''}); // 当前的词
+  const [range, setRange] = React.useState([5000,5100]); // 当前的词的范围
+  const [wordIndex, setWordIndex] = React.useState(getRandomNumberByRange(5000,5100)); // 当前的词的范围
+  const [record, setRecord] = React.useState(); // 答题记录
 
-  useEffect(() => {
-    console.log(id)
+   const setAWord = (id)=>{
     db.transaction(tx => {
       tx.executeSql(
         `select * from wordfeq where id = ?;`,
@@ -38,22 +41,56 @@ export default  function WordTestScreen(props) {
         (_, { rows: { _array } }) => setWord(_array[0])
       )
     })
-  }, []);
+  }
+
+  /**
+   * 下一词
+   */
+  const  nextWord = (res:boolean):void =>{
+    if(order+1 > 15){
+      Alert.alert('恭喜','新的词汇量已刷新');
+      return;
+    }
+    // 答对增加1000, 答错减1000
+
+    // 答错
+    setRange([wordIndex-1000,wordIndex-900])
+    // 答错
+    if(res){
+     setRange([wordIndex+1000,wordIndex+1100])
+    }
+
+  }
+
+  // 词汇量范围变的时候
+  useEffect(() => {
+    if(order+1 > 15){
+      Alert.alert('测试完毕','新的词汇量已刷新');
+      return;
+    }
+    setWordIndex(getRandomNumberByRange(range[0],range[1]));
+    setAWord(wordIndex)
+    setOrder(order+1)
+    setNum(Math.round(wordIndex/100)*100)
+  }, [range])
 
 
 
   return (
     <Layout style={styles.container}>
       <TopNavigation title={'当前词汇量: '+num}/>
+
       <View style={styles.container}>
-      <Text  style={styles.titleLabel} category='h1'>{word.name}</Text>
+  <Text  style={styles.titleLabel} category='h1'>{word.name}</Text>
+      <Text  style={styles.titleLabel} category='p1'>{range[0]}</Text>
+      <Text  style={styles.titleLabel} category='p1'>{range[1]}</Text>
+      <Text  style={styles.titleLabel} category='p1'>{wordIndex}</Text>
+      <Text  style={styles.titleLabel} category='p1'>{order}</Text>
       </View>
           <View style={styles.tabBarInfoContainer}>
             <View style={styles.buttonGroup}>
-              <Button style={styles.button} onPress={() => {
-              }} appearance='filled'>陌生</Button>
-              <Button style={styles.button} appearance='outline' onPress={() => {
-              }} >熟悉</Button>
+              <Button style={styles.button} onPress={() =>nextWord(false)} appearance='filled'>陌生</Button>
+              <Button style={styles.button} appearance='outline' onPress={() => nextWord(true)} >熟悉</Button>
             </View>
           </View>
     </Layout>
